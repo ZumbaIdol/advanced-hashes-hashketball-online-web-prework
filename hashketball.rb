1b7bfe84e50847ff3a127ba7f60a941431497b9e
David Foote <david.a.foote@gmail.com>
2019-06-10 00:34:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -123,7 +123,7 @@ def num_points_scored(players_name)
   hash.each do |location, info|
     info.each do |attribute, stuff|
       if stuff.include?(players_name)
-        return [info]
+        return [data_item]
       end
     end
   end
